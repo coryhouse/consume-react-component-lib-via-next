fbcbf994a2f1e665d920e83b1c4231966a1c702a
Cory House <housecor@gmail.com>
2025-01-27 07:10:13 -0600

    add

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { HelloWorld } from "corys-react-components";
+import { HelloWorld } from "@housecor/react-components";
 
 export default function Home() {
   return (
